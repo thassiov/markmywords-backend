1c97962d4aa31e48e9320332b4e197ad8c26963a
thassiov <tvmcarvalho@gmail.com>
2024-02-04 13:13:20 -0300

    Renaming files

--- src/models/selection/index.ts
+++ src/models/selection/index.ts
@@ -1,85 +1,13 @@
-import { DataTypes, Model } from 'sequelize';
-import { z } from 'zod';
-
-import { db } from '../db';
-
-class SelectionModel extends Model {}
-
-SelectionModel.init(
-  {
-    id: {
-      type: DataTypes.UUIDV4,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-    },
-    text: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    rawText: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-  },
-  { sequelize: db, modelName: 'selection' }
-);
-
-const selectionSchema = z.object({
-  id: z.string().uuid().optional(),
-  text: z.string().min(1),
-  rawText: z.string().min(1),
-  url: z.string().url(),
-});
-
-const getSelectionDtoSchema = selectionSchema;
-const createSelectionDtoSchema = selectionSchema.extend({
-  text: z.string().min(1).optional(),
-});
-
-type ISelection = z.infer<typeof selectionSchema>;
-type IGetSelectionDto = z.infer<typeof getSelectionDtoSchema>;
-type ICreateSelectionDto = z.infer<typeof createSelectionDtoSchema>;
-
-class SelectionDto {
-  static rawJsonToSelectionDto(selection: any): ICreateSelectionDto {
-    return {
-      rawText: selection.rawText,
-      text: selection.text || '',
-      url: selection.url,
-    };
-  }
-
-  static modelToGetSelection(selection: ISelection): IGetSelectionDto {
-    return {
-      id: selection.id ?? undefined,
-      text: selection.text,
-      rawText: selection.rawText,
-      url: selection.url,
-    };
-  }
-
-  static getSelectionDtoToModel(selection: IGetSelectionDto): ISelection {
-    return {
-      id: selection.id ?? undefined,
-      text: selection.text,
-      rawText: selection.rawText,
-      url: selection.url,
-    };
-  }
-
-  static createSelectionDtoToModel(selection: ICreateSelectionDto): ISelection {
-    return {
-      text: selection.text || '',
-      rawText: selection.rawText,
-      url: selection.url,
-    };
-  }
-}
+import {
+  ICreateSelectionDto,
+  IGetSelectionDto,
+  ISelection,
+  SelectionDto,
+  SelectionModel,
+  createSelectionDtoSchema,
+  getSelectionDtoSchema,
+  selectionSchema,
+} from './SelectionRepository';
 
 export {
   SelectionModel,
