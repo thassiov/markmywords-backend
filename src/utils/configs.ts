ae4302f973143d1cfa93c75c13b73cb995e671a6
thassiov <tvmcarvalho@gmail.com>
2024-02-12 22:04:43 -0300

    Add configs related to auth-auth

--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -8,10 +8,14 @@ const configs = {
   dbUser: process.env.DB_USER || 'localuser',
   dbPassword: process.env.DB_PASSWORD || 'password',
   dbHost: process.env.DB_HOST || 'localhost',
-  dbPort: process.env.DB_PORT || 5432,
+  dbPort: parseInt(process.env.DB_PORT as string) || 5432,
   dbName: process.env.DB_NAME || 'mmw',
-  apiPort: process.env.API_PORT || 8080,
+  apiPort: parseInt(process.env.API_PORT as string) || 8080,
   appEnvironment: process.env.NODE_ENV || 'development',
+  appMinAccountPasswordLength:
+    parseInt(process.env.APP_MIN_ACCOUNT_PASSWORD_LENGTH as string) || 8,
+  appAccountPasswordSaltGenRounds:
+    parseInt(process.env.APP_ACCOUNT_PASSWORD_SALT_GEN_ROUNDS as string) || 12,
 };
 
 export { configs };
